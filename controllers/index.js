d61b5a71cadc5e0abe500646114d6f42f95f801e
sophiekdj <s.dejong057@gmail.com>
2022-02-01 21:27:33 +0800

    added dashboard route

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
 
 // What's a homeroute????
 
 router.use('/', homeRoutes);
+router.use('/', dashboardRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
