addc94a5e5704fb0c21bfaafd69084091d62bf94
sophiekdj <s.dejong057@gmail.com>
2022-02-01 21:02:55 +0800

    added comment class and defined columns

---
+++ models/Comment.js
@@ -0,0 +1,56 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection.js");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    // columns defined 
+    // id of comment, contents of comment, date created
+    // post id of corresponding comment, commenter's user id
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // content of comment
+    content: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'post',
+            key: 'id',
+        },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+
+module.exports = Comment;
