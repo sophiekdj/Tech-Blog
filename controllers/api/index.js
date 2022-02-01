7f81edd40d86b1e169cb6f8953168d046bf97c20
sophiekdj <s.dejong057@gmail.com>
2022-02-01 21:29:45 +0800

    added comment route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
-const postRoutes = require('./postRoutes');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
-
-// shouldnt there be a route for comments?
+router.use('/comments', commentRoutes);
 
 module.exports = router;
+
+
+// overarching things
+// create new post, delete your post, UPDATE your post
+// create new comment, the post is then updated to display the comment
+// cannot delete or update comments
