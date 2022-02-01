6d441f0eb37e050d567ae1d4f9f2505d01e00f6c
sophiekdj <s.dejong057@gmail.com>
2022-02-01 20:17:32 +0800

    added skeleton post file to edit

--- models/Post.js
+++ models/Post.js
@@ -2,3 +2,52 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection.js");
 
 class Post extends Model {}
+
+Project.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // add post title, and contents of post, post creator's username, date created
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    // EDIT THIS???? to comments?????? comment has to be saved to a post
+    // what is datatypes.float
+    needed_funding: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+
+
+
+module.exports = Post;
